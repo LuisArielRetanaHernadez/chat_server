4a0763c3d9d203dc91331235db366cacbb0271da
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-19 22:22:06 -0600

    creando el model Menssage

---
+++ src/database/models/Menssage.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const menssageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  IDUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true
+  },
+  createDate: {
+    type: Date,
+    required: true
+  }
+})
+
+const Menssage = mongoose.model('Menssages', menssageSchema)
+
+module.exports = Menssage
