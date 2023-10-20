3cf39b398ef49e778cab5f42e7be5f17e2670b35
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-20 15:34:42 -0600

    creando la ruta api/v1/users y pasando el middleware error

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 
+// routers
+const { routerUser } = require('./src/routers/user.router')
+
+// middlewares
+const globalError = require('./src/middlewares/errors.middleware')
+
 const app = express()
+
+app.use(express.json())
+
+app.use('api/v1/users', routerUser)
+app.use(globalError)
 
 module.exports = app
