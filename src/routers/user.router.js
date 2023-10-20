df0e7298f3b2b1f778d7ca01a21fdcaed66902c1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-20 16:09:13 -0600

    creando el router login con el middleware loginUser

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,14 +1,15 @@
 const express = require('express')
 
 // middleware
-const { registerUser } = require('../middlewares/expressValidator/user.body')
+const { registerUser, loginUser } = require('../middlewares/expressValidator/user.body')
 const validate = require('../middlewares/validate.middleware')
 
 // controller
-const { register } = require('../controllers/user.controller')
+const { register, login } = require('../controllers/user.controller')
 
 const router = express.Router()
 
 router.post('/', validate(registerUser), register)
+router.post('/login', validate(loginUser), login)
 
 module.exports = { routerUser: router }
