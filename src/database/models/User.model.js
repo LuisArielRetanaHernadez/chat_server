4343cec89c5ad8138d3f9f3f679318b5bc07f4c4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-19 22:12:24 -0600

    creando el model users

---
+++ src/database/models/User.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  },
+  Age: {
+    type: Number,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: false
+  },
+  Contacts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  }]
+})
+
+const User = mongoose.model('Users', userSchema)
+
+module.exports = User
