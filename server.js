ec2f471b95881a9177333ad4e3e9707ebbe21e0b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-21 20:48:39 -0600

    agregando socket.io en el server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const app = require('./app')
 
+const { createServer } = require('http')
+
+const httpServer = createServer(app)
+
 const startServer = () => {
-  app.listen(() => {
+  httpServer.listen(3000, () => {
     console.log('Server is running on port 3000')
   })
 }
