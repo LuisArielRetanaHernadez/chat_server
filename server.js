f55a503e74235db3637bab4689df06222b3ae639
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-26 19:05:51 -0600

    importando la conexion de la base de datos

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const app = require('./app')
 
 const { createServer } = require('http')
+const connectDB = require('./src/database/database')
 
 const httpServer = createServer(app)
 
 const startServer = () => {
   httpServer.listen(3000, () => {
     console.log('Server is running on port 3000')
+    connectDB()
   })
 }
 
