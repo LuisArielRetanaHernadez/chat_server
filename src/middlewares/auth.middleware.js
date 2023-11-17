38d467a505f88ce30a852eac4ca8a938ae712912
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-17 17:26:08 -0600

    creando el middleware auth para verificar el token de la seccion

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const AppError = require('../utils/AppError')
+const tryCathc = require('../utils/tryCathc')
+
+// jsonwebtoken
+
+const jwt = require('jsonwebtoken')
+
+exports.authUser = tryCathc((req, res, next) => {
+  let token
+
+  if (req.headers.authorization &&
+  req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1]
+  } else {
+    return next(new AppError('Seccion invalida', 401))
+  }
+
+  const userCurrent = jwt.verify(token, process.env.SECRET_SECRET)
+
+  if (!userCurrent) {
+    return next(new AppError('Seccion invalida', 401))
+  }
+
+  req.userCurrent = userCurrent
+
+  next()
+})
