3a79151b403ae2f9f430ac0ca42ba48a09e21bbd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-17 17:30:03 -0600

    agregando el middleware al endpoint search

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -6,11 +6,12 @@ const validate = require('../middlewares/validate.middleware')
 
 // controller
 const { register, login, searchUsers } = require('../controllers/user.controller')
+const { authUser } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 router.post('/', validate(registerUser), register)
 router.post('/login', validate(loginUser), login)
-router.get('/search', searchUsers)
+router.get('/search', authUser, searchUsers)
 
 module.exports = { routerUser: router }
