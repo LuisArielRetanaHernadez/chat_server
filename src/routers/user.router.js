838c92ff6a6031b303dc29d57692f9212202b06c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-17 17:34:39 -0600

    cambiando la importancion de authUser por auth

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -6,12 +6,12 @@ const validate = require('../middlewares/validate.middleware')
 
 // controller
 const { register, login, searchUsers } = require('../controllers/user.controller')
-const { authUser } = require('../middlewares/auth.middleware')
+const { auth } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 router.post('/', validate(registerUser), register)
 router.post('/login', validate(loginUser), login)
-router.get('/search', authUser, searchUsers)
+router.get('/search', auth, searchUsers)
 
 module.exports = { routerUser: router }
