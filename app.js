da7c75d24a9f3fafe0f5c5292c0cbd9604ff5a0e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-02 17:40:38 -0600

    agregando la libreria cors en el app de express

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const { routerUser } = require('./src/routers/user.router')
 // middlewares
 const globalError = require('./src/middlewares/errors.middleware')
 
+// cors
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/api/v1/users', routerUser)
 app.use(globalError)
