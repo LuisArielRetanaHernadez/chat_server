7d8c58549d975cd0d8076daa69e33f54538536f6
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-08 15:59:51 -0600

    creando el middleware de autentificacion del namesspace users

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -1,24 +1,19 @@
+const User = require('../database/models/User.model')
+const AppError = require('../utils/AppError')
+const verifyToken = require('../utils/verifyToken')
+
 module.exports = (io) => {
-  io.of('/users').use((socket, next) => {
-    console.log('middleware 1', socket.handshake)
+  io.of('/users').use(async (socket, next) => {
+    const token = verifyToken(socket.handshake.query.token)
+    if (!token) {
+      return next(new AppError('Invalid token', 401))
+    }
+    console.log('token: ', token)
+    socket.user = token.id
     return next()
   })
 
   io.of('/users').on('connection', async (socket) => {
-    console.log('query: ', socket.handshake.query)
-    // if (socket.recovered) {
-    //   console.log('recovered ', socket.recovered)
-    // } else {
-    //   console.log('new connection', socket.recovered)
-    // }
-    socket.on('message', (message) => {
-      console.log(message)
-    })
-    socket.on('join', (room) => {
-      socket.join(room)
-    })
-    socket.on('disconnect', () => {
-      console.log('user disconnected')
-    })
+    console.log('informacion del query: ', socket.handshake.query)
   })
 }
