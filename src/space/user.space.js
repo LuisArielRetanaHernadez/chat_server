e58c3df260ba578b6b57ff75b89087122b1e4547
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-10 20:36:22 -0600

    creando el middleware para verificar el token

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -3,17 +3,20 @@ const AppError = require('../utils/AppError')
 const verifyToken = require('../utils/verifyToken')
 
 module.exports = (io) => {
-  io.of('/users').use(async (socket, next) => {
-    const token = verifyToken(socket.handshake.query.key)
-    if (!token) {
-      return next(new AppError('Invalid token', 401))
+  io.of('/users').use((socket, next) => {
+    try {
+      const token = verifyToken(socket.handshake.query.key)
+      if (!token) {
+        next(new AppError('Invalid token', 401))
+      }
+      socket.userId = token.id
+      return next()
+    } catch (error) {
+      next(new AppError('Invalid token', 401))
     }
-    socket.userId = token.id
-    return next()
   })
-
   io.of('/users').on('connection', (socket) => {
-    console.log('socket id ', socket.userId)
+    console.log('socket id /user', socket.id)
     console.log('informacion del query: ', socket.handshake.query)
   })
 }
