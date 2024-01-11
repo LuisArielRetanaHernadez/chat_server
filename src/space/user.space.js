b84df593aa4cd3194fc119b0e6a1f2fb082e9e4b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-11 16:46:31 -0600

    quitando el middleware para verificar el token en el espacio de nombre '/users'

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -3,18 +3,8 @@ const AppError = require('../utils/AppError')
 const verifyToken = require('../utils/verifyToken')
 
 module.exports = (io) => {
-  io.of('/users').use((socket, next) => {
-    try {
-      const token = verifyToken(socket.handshake.query.key)
-      socket.userId = token.id
-      return next()
-    } catch (error) {
-      const err = new AppError('not authorized')
-      err.data = { status: 401 }
-      next(err)
-    }
-  })
   io.of('/users').on('connection', (socket) => {
+    console.log('id socket ', socket.id)
     socket.on('users online', () => {
       const users = io.of('/users').sockets
       socket.emit('users online', users)
