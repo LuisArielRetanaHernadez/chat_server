51d5cfec146374bdf0e7d903ee2089cf0e56b925
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-10 19:43:12 -0600

    modificando la devolucion del error en la verificacion del token

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -4,11 +4,12 @@ const AppError = require('./AppError')
 require('dotenv').config()
 
 const verifyToken = (token) => {
-  const isTokenValid = jwt.verify(token, process.env.JW_SECRET)
-
-  if (!isTokenValid) {
-    throw new AppError('Invalid token', 401)
-  }
+  const isTokenValid = jwt.verify(token, process.env.JW_SECRET, (error, decoded) => {
+    if (error) {
+      throw new AppError('Invalid token', 401)
+    }
+    return decoded
+  })
 
   return isTokenValid
 }
