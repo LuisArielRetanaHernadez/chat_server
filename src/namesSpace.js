f9dfff87022dd6d6c5f7be9c5be8205b97fb5bc8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-10 20:37:44 -0600

    agregando la funcion userSpace pasandole el server io

--- src/namesSpace.js
+++ src/namesSpace.js
@@ -1,10 +1,14 @@
+const userSpace = require('./space/user.space')
+
 const namesSpace = (io) => {
   io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected')
+    console.log('socket id / ', socket.id)
     socket.onAny((event, ...args) => {
-      console.log(event, args);
+      console.log(event, args)
     })
   })
+  userSpace(io)
 }
 
 module.exports = namesSpace
