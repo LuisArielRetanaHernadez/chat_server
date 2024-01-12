825b5617622ef85d4fb5fc5bc921ca2b283aa303
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-11 21:13:57 -0600

    creando el middleware de autentificar el usuario

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -3,11 +3,23 @@ const AppError = require('../utils/AppError')
 const verifyToken = require('../utils/verifyToken')
 
 module.exports = (io) => {
+  io.of('/users').use(async (socket, next) => {
+    try {
+      const token = socket.handshake.auth.token
+      const user = await verifyToken(token)
+      if (!user) {
+        return next(new AppError('Invalid token', 401))
+      }
+      socket.user = user
+      next()
+    } catch (error) {
+      error.status = 401
+      next(error)
+    }
+  })
   io.of('/users').on('connection', (socket) => {
     console.log('id socket ', socket.id)
     socket.on('users online', () => {
-      const users = io.of('/users').sockets
-      socket.emit('users online', users)
     })
   })
 }
