3c0e97ecc0ba4e24a833340274cbee502fac7ccd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-12 20:10:19 -0600

    creando el evento ' users online'

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -3,23 +3,32 @@ const AppError = require('../utils/AppError')
 const verifyToken = require('../utils/verifyToken')
 
 module.exports = (io) => {
+  const clients = []
   io.of('/users').use(async (socket, next) => {
     try {
-      const token = socket.handshake.auth.token
-      const user = await verifyToken(token)
+      const token = socket.handshake.query.key
+      const user = verifyToken(token)
       if (!user) {
         return next(new AppError('Invalid token', 401))
       }
       socket.user = user
-      next()
+      return next()
     } catch (error) {
       error.status = 401
-      next(error)
+      return next(error)
     }
   })
   io.of('/users').on('connection', (socket) => {
-    console.log('id socket ', socket.id)
+    console.log('users connected', socket.user.id)
+    const socketInformation = {
+      userId: socket.user.id
+    }
+    if (!clients.find((client) => client.userId === socketInformation.userId)) {
+      clients.push(socketInformation)
+    }
     socket.on('users online', () => {
+      const usersOnline = clients.filter((client) => client.userId !== socketInformation.userId)
+      socket.emit('users online', usersOnline)
     })
   })
 }
