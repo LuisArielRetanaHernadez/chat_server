2770ad2784ca8fe6b670918f6b7d3876fe2e84a8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-20 21:18:57 -0600

    elimando el middleware del user space

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -1,22 +1,5 @@
-const AppError = require('../utils/AppError')
-const verifyToken = require('../utils/verifyToken')
-
 module.exports = (io) => {
   const clients = []
-  io.of('/users').use(async (socket, next) => {
-    try {
-      const token = socket.handshake.query.key
-      const user = verifyToken(token)
-      if (!user) {
-        return next(new AppError('Invalid token', 401))
-      }
-      socket.user = user
-      return next()
-    } catch (error) {
-      error.status = 401
-      return next(error)
-    }
-  })
   io.of('/users').on('connection', (socket) => {
     const socketInformation = {
       userId: socket.user.id
