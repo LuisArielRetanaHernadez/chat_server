a7133f79ba08ec67008184655d6ac34600fa536c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-21 13:59:11 -0600

    creadno el middleware 'game join'

--- src/middlewares/socket/user/socketUser.middleware.js
+++ src/middlewares/socket/user/socketUser.middleware.js
@@ -1,6 +1,18 @@
+const jwt = require('jsonwebtoken')
+const AppError = require('../../../utils/AppError')
+
 exports.userMiddleware = {
   'send message': async (socket, data, next) => {
-    console.log('send message')
+    const token = socket.handshake.query.token
+    if (!token) {
+      return next(new AppError('no token', 404))
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return next(new AppError('token invalid', 404))
+      }
+      return decoded
+    })
     next()
   }
 }
