524820cb5a7cd8f7ddfd55551060e0c049633300
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-20 21:01:17 -0600

    elimnando el evento general en el namesSpace

--- src/namesSpace.js
+++ src/namesSpace.js
@@ -1,13 +1,6 @@
 const userSpace = require('./space/user.space')
 
 const namesSpace = (io) => {
-  io.on('connection', (socket) => {
-    console.log('a user connected')
-    console.log('socket id / ', socket.id)
-    socket.onAny((event, ...args) => {
-      console.log(event, args)
-    })
-  })
   userSpace(io)
 }
 
