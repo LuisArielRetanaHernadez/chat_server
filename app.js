cf7817b25d4146f41928e4420fb7aec461ff3e82
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-22 21:05:45 -0600

    agregando el router '/api/v1/messages' con el routerMessage

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 // routers
 const { routerUser } = require('./src/routers/user.router')
+const { routerMessage } = require('./src/routers/message.router')
 
 // middlewares
 const globalError = require('./src/middlewares/errors.middleware')
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/v1/users', routerUser)
+app.use('/api/v1/messages', routerMessage)
 app.use(globalError)
 
 module.exports = app
