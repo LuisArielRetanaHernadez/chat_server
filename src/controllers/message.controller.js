1fb9a32afe545884e777b8181b36ba8b7115150f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-01 21:26:38 -0600

    cambiando los codigo de estado del rest del controller saveMessage

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -2,7 +2,7 @@ const Chat = require('../database/models/Chat.model')
 const Menssage = require('../database/models/Menssage.model')
 
 exports.saveMessage = async (req, res, next) => {
-  const { message } = req.body
+  const { message, isGroup } = req.body
 
   const newMessage = Menssage.create({
     content: message,
@@ -14,7 +14,7 @@ exports.saveMessage = async (req, res, next) => {
     por lo cual el campo users debe tener 2 usuarios y los id de los
     usuarios del chat
   */
-  const chat = await Chat.findOne({ users: [req.user.id, req.body.id], isGroup: true })
+  const chat = await Chat.findOne({ users: [req.user.id, req.body.id], isGroup })
 
   if (!chat) {
     const newChat = Chat.create({
@@ -23,8 +23,8 @@ exports.saveMessage = async (req, res, next) => {
       isGroup: true,
       messagesIds: [newMessage]
     })
-    return res.status(200).json({
-      message: 'chat created',
+    return res.status(201).json({
+      message: 'send message',
       data: newChat,
       status: 'succes'
     })
@@ -35,8 +35,8 @@ exports.saveMessage = async (req, res, next) => {
   chat.messages.push(newMessage)
   await chat.save()
 
-  return res.status(200).json({
-    message: 'chat created',
+  return res.status(201).json({
+    message: 'send message',
     data: chat,
     status: 'succes'
   })
