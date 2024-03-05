d99dad172e3a16639ef352db053c406f6cffda28
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-04 18:12:46 -0600

    creando la funcion verifyConnect para verificar si la conexion del cliente es valida y creamos el middleware para guadar el id de la base de datos y el id socket de forma paralela

--- src/space/user.space.js
+++ src/space/user.space.js
@@ -1,8 +1,46 @@
 const User = require('../database/models/User.model')
+const AppError = require('../utils/AppError')
+const jwt = require('jsonwebtoken')
 
-const clients = []
+const clients = {}
+
+const verifyConnect = async (socket) => {
+  const token = socket.handshake.query.key
+
+  if (!token) {
+    throw new AppError('token invalid', 404)
+  }
+
+  const decoded = jwt.verify(token, process.env.JW_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      throw new AppError('token invalid', 404)
+    }
+
+    return decoded
+  })
+
+  const user = await User.exists({ _id: decoded.id })
+
+  if (!user) {
+    throw new AppError('user not found', 404)
+  }
+
+  if (user) return decoded.id
+  return null
+}
 
 module.exports = (io) => {
+  io.of('/users').use(async (socket, next) => {
+    const user = await verifyConnect(socket)
+      .then((id) => id)
+      .catch((err) => next(err))
+
+    if (!user) return next(new AppError('user not found', 404))
+    clients[user] = socket.id
+
+    next()
+  })
   io.of('/users').on('connection', (socket) => {
     socket.on('users online', async () => {
       const usersOnline = await User.find({ isOnline: true })
@@ -14,8 +52,8 @@ module.exports = (io) => {
     })
 
     socket.on('send message', (data) => {
-      console.log(data)
-      socket.to(clients[0].socketID).emit('message', data)
+      console.log(clients[data.to])
+      socket.to(clients[data.to]).emit('message', data)
     })
   })
 }
