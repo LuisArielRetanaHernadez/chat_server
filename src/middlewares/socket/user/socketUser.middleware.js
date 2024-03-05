3a295db6073e994fb6590240266b729d57107f5c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-05 14:18:13 -0600

    cambiando el mensaje y codigo de la seccion invalidad del middleware 'send message'

--- src/middlewares/socket/user/socketUser.middleware.js
+++ src/middlewares/socket/user/socketUser.middleware.js
@@ -5,11 +5,11 @@ exports.userMiddleware = {
   'send message': async (socket, data, next) => {
     const token = socket.handshake.query.key
     if (!token) {
-      return next(new AppError('no token', 404))
+      return next(new AppError('Seccion Invalida', 401))
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        return next(new AppError('token invalid', 404))
+        return next(new AppError('Seccion Invalida', 401))
       }
       return decoded
     })
