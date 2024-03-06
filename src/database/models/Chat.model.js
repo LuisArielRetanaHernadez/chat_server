20c04eaf4306ffd8169ce8c60f330859c47219c9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-05 18:07:40 -0600

    cambiando los nombres de la referencias a los demas documentos por plural y la referencia 'Mensaage' por 'Message'

--- src/database/models/Chat.model.js
+++ src/database/models/Chat.model.js
@@ -11,7 +11,7 @@ const chatSchema = new mongoose.Schema({
   }],
   messagesIds: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Menssage'
+    ref: 'Message'
   }],
   isGroup: {
     type: Boolean,
