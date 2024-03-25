4196f3a39bd46ba2865d83d52ab0ba32729be863
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-25 16:25:42 -0600

    creando el router '/contacts' para obtener los usuarios

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -5,14 +5,18 @@ const { registerUser, loginUser } = require('../middlewares/expressValidator/use
 const validate = require('../middlewares/validate.middleware')
 
 // controller
-const { register, login, searchUsers, addContact } = require('../controllers/user.controller')
+const { register, login, searchUsers, addContact, getContacts } = require('../controllers/user.controller')
 const { auth } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
+// user
 router.post('/register', validate(registerUser), register)
 router.post('/login', validate(loginUser), login)
+// search users
 router.get('/search', auth, searchUsers)
+// contacts
 router.put('/addContact/:id', auth, addContact)
+router.get('/contacts', auth, getContacts)
 
 module.exports = { routerUser: router }
