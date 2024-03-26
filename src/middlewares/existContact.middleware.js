7563818991eafc6aff5be2d922319e1499962a1c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-26 14:02:48 -0600

    cambiando la condicion para validar si el contacto debe de existir o no en el middleware existContact

--- src/middlewares/existContact.middleware.js
+++ src/middlewares/existContact.middleware.js
@@ -8,13 +8,21 @@ exports.existContact = (isPass = false) => {
     const { id } = req.params
 
     const isContact = await User.findOne({
-      _id: id,
-      'contacts._id': userCurrent._id
+      id: userCurrent.id,
+      'contacts._id': id
     }).select('contacts')
 
+    console.log(isContact)
+
     if (!isPass) {
+      if (isContact !== null) {
+        return next(new AppError('Contact already exist', 404))
+      }
+    }
+
+    if (isPass) {
       if (isContact === null) {
-        return next(new AppError('contact does not exist', 401))
+        return next(new AppError('Contact not exist', 404))
       }
     }
 
