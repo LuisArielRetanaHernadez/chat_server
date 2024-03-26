ca4210f3a6584ef9926dc9f7f12fdaf661085eb5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-26 16:41:04 -0600

    creando el router '/user/:id/' con el controlador getUser

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -5,7 +5,7 @@ const { registerUser, loginUser } = require('../middlewares/expressValidator/use
 const validate = require('../middlewares/validate.middleware')
 
 // controller
-const { register, login, searchUsers, addContact, getContacts, getContact } = require('../controllers/user.controller')
+const { register, login, searchUsers, addContact, getContacts, getContact, getUser } = require('../controllers/user.controller')
 const { auth } = require('../middlewares/auth.middleware')
 const { existContact } = require('../middlewares/existContact.middleware')
 
@@ -14,6 +14,7 @@ const router = express.Router()
 // user
 router.post('/register', validate(registerUser), register)
 router.post('/login', validate(loginUser), login)
+router.get('/user/:id', auth, getUser)
 // search users
 router.get('/search', auth, searchUsers)
 // contacts
