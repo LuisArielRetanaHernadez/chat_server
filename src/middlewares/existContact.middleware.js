1dd8b9cc1dc3db70dc16d0043c73a6c25921d594
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-25 23:18:43 -0600

    pasando el valor false como argumento por defecto al middleware existContact

--- src/middlewares/existContact.middleware.js
+++ src/middlewares/existContact.middleware.js
@@ -2,7 +2,7 @@ const User = require('../database/models/User.model')
 const AppError = require('../utils/AppError')
 const tryCathc = require('../utils/tryCathc')
 
-exports.existContact = (isPass) => {
+exports.existContact = (isPass = false) => {
   return tryCathc(async (req, res, next) => {
     const { userCurrent } = req
     const { id } = req.params
