5e4f1e0ba76d0da4018739e35ade29bb022c6150
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-29 22:16:39 -0600

    arreglando la importacion de los controladores para el extraelos desde el archivo controladores user

--- src/routers/contacts.routers.js
+++ src/routers/contacts.routers.js
@@ -1,6 +1,6 @@
-const { addContact, getContacts, getContact } = require("../controllers/user.controller")
-const { auth } = require("../middlewares/auth.middleware")
-const { existContact } = require("../middlewares/existContact.middleware")
+const { addContact, getContacts, getContact } = require('../controllers/contact.controller')
+const { auth } = require('../middlewares/auth.middleware')
+const { existContact } = require('../middlewares/existContact.middleware')
 
 const express = require('express')
 
@@ -8,7 +8,7 @@ const router = express.Router()
 
 // contacts
 router.get('/', auth, existContact(false), getContacts)
-router.put('/addContact/:id', auth, existContact(false), addContact)
+router.put('/add/:id', auth, existContact(false), addContact)
 router.get('/contacts/:id', auth, getContact)
 
 module.exports = { routerContact: router }
