90a2aa193c2831b2885469ec2ade154d90acefe2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-01 17:52:29 -0600

    creando la funcionalidad para comprobar si el nombre del template pasado existe

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -1,6 +1,26 @@
+import AppError from '../AppError'
 import { transport } from '../nodemailer/transport'
 
+const fs = require('fs')
+const path = require('path')
+
 const sendEmail = (to, from, subject, content, templateName) => {
+  const dirCurrent = __dirname
+  // const dirTemplate = path.join(dirCurrent, 'templates')
+
+  // fs.readFile(path.join(dirCurrent, 'templates', templateName), 'utf8', (err, html) => {
+  //   if (err) throw err
+  //   content = html
+  // })
+
+  fs.readdir(dirCurrent, (err, files) => {
+    if (err) throw AppError(err, 404)
+    const foldersName = files.filter(file => fs.lstatSync(path.join(dirCurrent, file)).isDirectory())
+    if (!foldersName.includes(templateName)) {
+      throw AppError('template not found', 404)
+    }
+  })
+
   transport.send({
     template: templateName,
     message: {
