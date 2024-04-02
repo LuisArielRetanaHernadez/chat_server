2ebdc5132a8b5bf71ae2dacfbe98d60d15352c9e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-01 21:39:49 -0600

    creando el campo user en el model CheckEmail

--- src/database/models/CheckEmail.model.js
+++ src/database/models/CheckEmail.model.js
@@ -9,6 +9,10 @@ const checkEmailSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
   code: {
     type: String,
     required: true,
