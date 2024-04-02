9d93d44e3e8cbd8edf4025e3025718a28e70746f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-02 14:40:36 -0600

    creando el router '/check/email/:token' con el controlador checkEmail

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -5,13 +5,14 @@ const { registerUser, loginUser } = require('../middlewares/expressValidator/use
 const validate = require('../middlewares/validate.middleware')
 
 // controller
-const { register, login, searchUsers, getUser } = require('../controllers/user.controller')
+const { register, login, searchUsers, getUser, checkEmail } = require('../controllers/user.controller')
 const { auth } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 // user
 router.post('/register', validate(registerUser), register)
+router.post('/check/email/:token', checkEmail)
 router.post('/login', validate(loginUser), login)
 router.get('/search', auth, searchUsers)
 router.get('/:id', auth, getUser)
