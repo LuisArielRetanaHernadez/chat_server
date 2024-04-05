08af394d106e03a1d1faf02fea149cbfbccb3068
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-05 16:08:32 -0600

    creando el controlador para crear la firma del cloudinary

---
+++ src/controllers/cloudinary.controller.js
@@ -0,0 +1,14 @@
+const cloudinary = require('../utils/cloudinary/cloudinary')
+const tryCathc = require('../utils/tryCathc')
+
+exports.createSignCloudinary = tryCathc(async (req, res) => {
+  const timestamp = Math.round((new Date()).getTime() / 1000)
+  const signature = await cloudinary.utils.api_sign_request({
+    timestamp
+    // source: req.body.source,
+    // folder: req.body.folder,
+    // public_id: req.body.public_id,
+    // api_key: process.env.CLOUDINARY_API_KEY
+  }, process.env.CLOUDINARY_API_SECRET)
+  res.json({ signature, timestamp })
+})
