000d5c6e483af99f891690c46e6432d038e4667e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-05 19:33:32 -0600

    cambiando el nombre del archvi para verificar el usuario a 'protectUser'

--- src/middlewares/protectUser.middleware.js
+++ src/middlewares/protectUser.middleware.js
@@ -1,7 +1,7 @@
 const AppError = require('../utils/AppError')
 const tryCathc = require('../utils/tryCathc')
 
-exports.protectMiddleware = tryCathc(async (req, res, next) => {
+exports.protectUser = tryCathc(async (req, res, next) => {
   const { userCurrent } = req
   const { id } = req.params
 
