550cd1f6ba4f89cd7d6bcc7b2442eedb8c91b87d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-05 19:06:06 -0600

    creancion del middleware protectedMiddleware

---
+++ src/middlewares/protectUser.middleware.js
@@ -0,0 +1,12 @@
+const AppError = require('../utils/AppError')
+const tryCathc = require('../utils/tryCathc')
+
+exports.protectMiddleware = tryCathc(async (req, res, next) => {
+  const { userCurrent } = req
+  const { id } = req.params
+
+  if (userCurrent.id !== id) {
+    return next(new AppError('error protected error', 401))
+  }
+  return next()
+})
