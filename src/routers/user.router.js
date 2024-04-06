351997775f6da851492c5368735d844b8bf6f54a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-05 18:29:22 -0600

    creando el router para el controlador de guardar la imagen de perfil del usuario

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -5,13 +5,14 @@ const { registerUser, loginUser } = require('../middlewares/expressValidator/use
 const validate = require('../middlewares/validate.middleware')
 
 // controller
-const { register, login, searchUsers, getUser, verifyEmail, verifyTokenEmail, resendCodeEmail } = require('../controllers/user.controller')
+const { register, login, searchUsers, getUser, verifyEmail, verifyTokenEmail, resendCodeEmail, uploadPhotoProfile } = require('../controllers/user.controller')
 const { auth } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 // user
 router.post('/register', validate(registerUser), register)
+router.post('/upload/image/profile', uploadPhotoProfile)
 router.post('/verify/email/:token', verifyEmail)
 router.post('/login', validate(loginUser), login)
 router.get('/search', auth, searchUsers)
