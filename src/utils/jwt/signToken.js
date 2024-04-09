b88f6efba77acafdc51baed59a71729f91d45a7e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-09 17:09:35 -0600

    corregimos la importacion del jsonwebtoken en el archivo signToken

--- src/utils/jwt/signToken.js
+++ src/utils/jwt/signToken.js
@@ -1,4 +1,4 @@
-import * as jwt from 'jsonwebtoken'
+const jwt = require('jsonwebtoken')
 
 const signToken = (payload, expiers = '1h') => {
   return jwt.sign(payload, process.env.JW_SECRET, { expiresIn: expiers })
