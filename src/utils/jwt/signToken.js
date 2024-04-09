c81e6df5a6c2306da96c91c5e84d827f9c26864c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-09 16:54:57 -0600

    creando el parametro expiers con el valor '1h' por defecto en la funcion signToken

--- src/utils/jwt/signToken.js
+++ src/utils/jwt/signToken.js
@@ -1,7 +1,7 @@
 import * as jwt from 'jsonwebtoken'
 
-const signToken = (payload) => {
-  return jwt.sign(payload, process.env.JW_SECRET, { expiresIn: '1h' })
+const signToken = (payload, expiers = '1h') => {
+  return jwt.sign(payload, process.env.JW_SECRET, { expiresIn: expiers })
 }
 
 module.exports = signToken
