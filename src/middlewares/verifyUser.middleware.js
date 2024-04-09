05ae3eeb89c4bfcd67de42d3a585a37aff6eeca3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-08 18:59:52 -0600

    agregando el await en el middleware verifyUser

--- src/middlewares/verifyUser.middleware.js
+++ src/middlewares/verifyUser.middleware.js
@@ -4,9 +4,9 @@ const tryCathc = require('../utils/tryCathc')
 
 exports.verifyUser = tryCathc(async (req, res, next) => {
   const { userCurrent } = req
-  const user = User.findById(userCurrent.id)
+  const user = await User.findById(userCurrent.id)
   if (!user) {
-    return next(new AppError('User Unknown', 401))
+    return next(new AppError('User not found', 401))
   }
   next()
 })
