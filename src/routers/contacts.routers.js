efaed5e7396f9ec7da626ead7a3f229af3ecb27c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-12 21:27:53 -0600

    creando el router 'is/contact/:id' con el controlador isContact

--- src/routers/contacts.routers.js
+++ src/routers/contacts.routers.js
@@ -1,4 +1,4 @@
-const { addContact, getContacts, getContact } = require('../controllers/contact.controller')
+const { addContact, getContacts, getContact, isContact } = require('../controllers/contact.controller')
 const { auth } = require('../middlewares/auth.middleware')
 const { existContact } = require('../middlewares/existContact.middleware')
 
@@ -8,6 +8,7 @@ const router = express.Router()
 
 // contacts
 router.get('/', auth, getContacts)
+router.get('/is/contact/:id', auth, isContact)
 router.put('/add/:id', auth, existContact(false), addContact)
 router.get('/contacts/:id', auth, getContact)
 
