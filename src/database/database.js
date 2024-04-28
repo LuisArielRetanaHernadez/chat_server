7b81b4bcd14b47b5915d8954d86bc0fff6d582c8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-27 22:26:10 -0600

    elimando los console.log

--- src/database/database.js
+++ src/database/database.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const connectDB = async () => {
-  console.log('Connecting to MongoDB... ', process.env.MONGO_URL)
   const connect = await mongoose.connect(process.env.MONGO_URL)
-  console.log(`MongoDB Connected: ${connect}`)
   return connect
 }
 
