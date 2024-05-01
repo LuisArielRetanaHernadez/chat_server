b35b2109866fd1a3ae85e045735e0476e7a64146
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-30 18:05:23 -0600

    elimando el middleware list chat

--- src/middlewares/socket/user/socketUser.middleware.js
+++ src/middlewares/socket/user/socketUser.middleware.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken')
 const AppError = require('../../../utils/AppError')
-const verifyToken = require('../../../utils/jwt/verifyToken')
-const ListChat = require('../../../database/models/ListChat')
 
 exports.userMiddleware = {
   'send message': async (socket, data, next) => {
@@ -15,16 +13,6 @@ exports.userMiddleware = {
       }
       return decoded
     })
-    next()
-  },
-  'list chat': async (socket, data, next) => {
-    const token = socket.handshake.query.key
-    const decoded = verifyToken(token)
-
-    const listChat = await ListChat.findOne({ user: decoded.id })
-    if (listChat === null) {
-      await ListChat.create({ user: decoded.id })
-    }
     next()
   }
 }
